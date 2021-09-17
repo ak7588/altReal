0c50ace9a0d3cfee2d63fc6f62eb79397bed2c9b
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-09-17 13:53:18 +0400

    Add animation

--- Sep15Lab/Assets/Scripts/LerpAnimation.cs
+++ Sep15Lab/Assets/Scripts/LerpAnimation.cs
@@ -4,25 +4,28 @@ using UnityEngine;
 
 public class LerpAnimation : MonoBehaviour
 {
-    public Vector3 start = Vector3.zero;
-    public Vector3 end = Vector3.zero;
+    public Transform start;
+    public Transform end;
     public float duration = 4;
     float currentPos = 0;
+    AnimationCurve curve = AnimationCurve.EaseInOut(0,0,1,1);
 
     // Start is called before the first frame update
     void Start()
     {
         currentPos = 0;
-        transform.position = start;
+        transform.position = start.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position != end) {
+        if (transform.position != end.position) {
             currentPos += Time.deltaTime / duration;
             currentPos = Mathf.Clamp01(currentPos);
-            transform.position = Vector3.Lerp(start, end, currentPos);
+            float t = curve.Evaluate(currentPos);
+            transform.position = Vector3.Lerp(start.position, end.position, t);
+            transform.rotation = Quaternion.Slerp(start.rotation, end.rotation, t);
         }
     }
 }
