db667dcc1d2164c2b2a9093dd87642ac66f717da
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-10-25 15:08:27 +0400

    Add scripts and interactions

---
+++ Oct25Lab/Assets/Scripts/XRSceneTransitionManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using System.Linq;
+
+[DisallowMultipleComponent]
+
+public class XRSceneTransitionManager : MonoBehaviour
+{
+    public static XRSceneTransitionManager Instance;
+
+    public bool isLoading { get; private set; } = false;
+
+    Scene xrScene;
+    Scene currentScene;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        } else {
+            Debug.LogWarning("Detected rouge XRSceneTransitionManager. Deleting it.");
+            Destroy(this.gameObject);
+            return;
+        }
+
+        xrScene = SceneManager.GetActiveScene();
+        SceneManager.sceneLoaded += OnNewSceneAdded;
+    }
+
+    public void TransitionTo(string scene) {
+        if (!isLoading) {
+            StartCoroutine(Load(scene));
+        }
+    }
+
+    void OnNewSceneAdded(Scene newScene, LoadSceneMode mode) {
+        if (newScene != xrScene) {
+            currentScene = newScene;
+            SceneManager.SetActiveScene(currentScene);
+            PlaceXRRig(xrScene, currentScene);
+        }
+    }
+
+    IEnumerator Load(string scene) {
+        isLoading = true;
+        yield return StartCoroutine(UnloadCurrentScene());
+        yield return StartCoroutine(LoadNewScene(scene));
+        isLoading = false;
+    }
+
+    IEnumerator UnloadCurrentScene() {
+        AsyncOperation unload = SceneManager.UnloadSceneAsync(currentScene);
+        while (!unload.isDone) {
+            yield return null;
+        }
+    }
+
+    IEnumerator LoadNewScene(string scene)
+    {
+        AsyncOperation load = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+        while (!load.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    static public void PlaceXRRig(Scene xrScene, Scene newScene)
+    {
+        GameObject[] xrObjects = xrScene.GetRootGameObjects();
+        GameObject[] newSceneObjects = newScene.GetRootGameObjects();
+
+        GameObject xrRig = xrObjects.First((obj) => { return obj.CompareTag("XRRig"); });
+        GameObject xrRigOrigin = newSceneObjects.First((obj) => { return obj.CompareTag("XRRigOrigin"); });
+
+        if (xrRig && xrRigOrigin)
+        {
+            xrRig.transform.position = xrRigOrigin.transform.position;
+            xrRig.transform.rotation = xrRigOrigin.transform.rotation;
+        }
+    }
+}
