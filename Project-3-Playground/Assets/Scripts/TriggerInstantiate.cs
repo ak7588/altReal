d4b86084e9b460efac0017f6da772477d674a740
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2021-11-10 15:58:51 +0400

    Work in progress

--- Project-3-Playground/Assets/Scripts/TriggerInstantiate.cs
+++ Project-3-Playground/Assets/Scripts/TriggerInstantiate.cs
@@ -1,42 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime;
+using Random = System.Random;
 
 public class TriggerInstantiate : MonoBehaviour
 {
     public GameObject projectilePrefab;
     public float launchForce = 100.0f;
     public Transform launchSpawn;
+    private double maxNumber = 7.22;
+    private double minNumber = -6.52;
 
-    private void OnTriggerEnter(Collider other)
+    private void Awake()
     {
-        if (other.tag == "Player")
+        for(int i = 0; i < 20; i++)
         {
-            StartCoroutine("CloneCube");
+            Launch();
         }
     }
 
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Player")
+    //    {
+    //        Debug.Log("Here");
+    //        //StartCoroutine("CloneCube");
+    //        Launch();
+    //    }
+    //}
+
     public void Launch()
     {
-        GameObject projectile = GameObject.Instantiate(projectilePrefab, launchSpawn.position, launchSpawn.rotation);
+        Vector3 spawn = new Vector3(new Random().NextDouble() * (maxNumber - minNumber) + minNumber, 0, new Random().NextDouble() * (maxNumber - minNumber) + minNumber);
+        GameObject projectile = GameObject.Instantiate(projectilePrefab, spawn, launchSpawn.rotation);
         Rigidbody rb = projectile.GetComponent<Rigidbody>();
         rb.AddForce(projectile.transform.forward * launchForce);
         HighLightController hc = projectile.GetComponent<HighLightController>();
         hc.SetBaseColor(Random.ColorHSV());
     }
 
-    IEnumerator CloneCube()
-    {
-        int count = 0;
-        while (count < 20)
-        {
-            GameObject projectile = GameObject.Instantiate(projectilePrefab, launchSpawn.position, launchSpawn.rotation);
-            Rigidbody rb = projectile.GetComponent<Rigidbody>();
-            rb.AddForce(projectile.transform.forward * launchForce);
-            HighLightController hc = projectile.GetComponent<HighLightController>();
-            hc.SetBaseColor(Random.ColorHSV());
-            count += 1;
-        }
-        yield return null;
-    }
+    //IEnumerator CloneCube()
+    //{
+    //    int count = 0;
+    //    while (count < 20)
+    //    {
+    //        GameObject projectile = GameObject.Instantiate(projectilePrefab, launchSpawn.position, launchSpawn.rotation);
+    //        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+    //        rb.AddForce(projectile.transform.forward * launchForce);
+    //        HighLightController hc = projectile.GetComponent<HighLightController>();
+    //        hc.SetBaseColor(Random.ColorHSV());
+    //        count += 1;
+    //    }
+    //    yield return null;
+    //}
 }
